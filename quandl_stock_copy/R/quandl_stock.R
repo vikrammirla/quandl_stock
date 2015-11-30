7b575f2b3e2356f623597611bfa449a48824789e
vikrammirla <vikrammirla@gmail.com>
2015-11-29 21:26:25 -0400

    fix for desription

---
+++ quandl_stock_copy/R/quandl_stock.R
@@ -0,0 +1,10 @@
+
+quandl_stock = function() {
+
+	library(Quandl)
+	Quandl.api_key("pk6yzCsxF1k-ygB29nfS")
+	data<-Quandl(c("NS1/CSCO_MM"), trim_start="2014-11-13")
+	plot(data[,4])
+    invisible();
+}
+
